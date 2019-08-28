b23bb1f3f913e1b965f3a670417c0232b6fe5ea1
Tebogo Phasha <tebogo@audetmagna.co.uk>
2019-08-28 22:36:55 +0200

    first commit

---
+++ employeemanagement-master/backend/ejbModule/service/EmployeeEJB.java
@@ -0,0 +1,39 @@
+package service;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import entities.EmployeeEntity;
+
+/**
+ * Session Bean implementation class EmployeeEJB
+ */
+@Stateless
+@LocalBean
+public class EmployeeEJB {
+
+
+	@PersistenceContext
+	private EntityManager em;
+	
+    public EmployeeEJB() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    public void addNew(EmployeeEntity employeeEntity)
+    {
+    	System.out.println("============================");
+    	System.out.println(employeeEntity.getName());
+    	em.persist(employeeEntity);
+    	System.out.println("============================");
+    }
+    
+    
+    public List<EmployeeEntity> findEmployees(){
+    	TypedQuery<Employee> query = em.createNamedQuery("findAllEmployees", Employee.Class);
+    	return query.getResults;
+    }
+
+}
